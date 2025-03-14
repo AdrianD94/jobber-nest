445bc99f07a6aa2a4aa5932cfe0253f1595fe4ec
adriand94 <ady.drozman2007@gmail.com>
2025-03-14 10:34:23 +0200

    chore: added config module

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     UsersModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
